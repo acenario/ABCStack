92e4be6fdb67a0ea301b10b5f7331590383128f2
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-05-06 17:25:31 +0000

    removed old code

--- lcd.py
+++ lcd.py
@@ -20,8 +20,6 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_c
 
 
 def LCD_Write(string, blink_cursor=False):
-        string = translate_to_lines(string)
-	#Clear the LCD
 	lcd.clear()
 	lcd.show_cursor(False)
 	lcd.blink(False)
